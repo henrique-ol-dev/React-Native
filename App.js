d3b6aa6232925a5fa800a84dc2c94e0c604cd08a
FIC <FIC@info.local>
2020-11-12 03:06:50 -0300

    começo

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const logar = () => {
+    alert('${email} ${senha}');
+
+    fetch('', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      }
+    }).then(response => response.json())
+      .then(data => alert(data));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>MOOOBIILEEE</Text>
+
+      <Text>Email</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+
+      <Text>Senha</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setSenha(text)}
+        secureTextEntry={true}
+        value={senha}
+      />
+
+      <Button
+        onPress={logar}
+        title="Login"
+        color="#841584"
+        accessibilityLabel="Login"
+      />
       <StatusBar style="auto" />
     </View>
   );
